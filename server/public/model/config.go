4d032941a52b3c6e9594b5b7b9d6ddd68c341fd3
Brandon Dobbie <brandon@fox-labs.io>
2023-06-13 00:04:21 -0400

    Started adding GraphQL and configuration for other services (SQL, file uploads, etc.)

--- server/public/model/config.go
+++ server/public/model/config.go
@@ -21,23 +21,74 @@ const (
 	ServiceSettingsDefaultIdleTimeout   = 60
 )
 
+type Config struct {
+	ServiceSettings   ServiceSettings
+	SqlSettings       SqlSettings
+	FileSettings      FileSettings
+	EmailSettings     EmailSettings
+	RateLimitSettings RateLimitSettings
+	OpenIdSettings    OpenIdSettings
+}
+
 type ServiceSettings struct {
 	SiteURL                  *string `access:"environment_web_server,authentication,write_restrictable"`
 	ListenAddress            *string `access:"environment_web_server,write_restrictable"`
 	ConnectionSecurity       *string `access:"environment_web_server,write_restrictable"`
 	TLSCertFile              *string `access:"environment_web_server,write_restrictable"`
 	TLSKeyFile               *string `access:"enviroment_web_server,write_restrictable"`
-	TLSMinVer                *string `access:"write_restrictable"`
-	TLSStrictTransport       *bool   `access:"write_restrictable"`
-	TLSStrictTransportMaxAge *int64  `access:"write_restrictable"`
+	TLSMinVer                *string `access:"environment_web_server,write_restrictable"`
+	TLSStrictTransport       *bool   `access:"environment_web_server,write_restrictable"`
+	TLSStrictTransportMaxAge *int64  `access:"environment_web_server,write_restrictable"`
 	UseLetsEncrypt           *bool   `access:"environment_web_server,write_restrictable"`
 	LetsEncryptCertCacheFile *string `access:"environment_web_server,write_restrictable"`
 	ReadTimeout              *int    `access:"environment_web_server,write_restrictable"`
 	WriteTimeout             *int    `access:"environment_web_server,write_restrictable"`
 	IdleTimeout              *int    `access:"environment_web_server,write_restrictable"`
 	ForwardHTTPToHTTPS       *bool   `access:"environment_web_server,write_restrictable"`
-	AllowCorsFrom            *string `access:"write_restrictable"`
+	AllowCorsFrom            *string `access:"environment_web_server,write_restrictable"`
 	EnableDeveloper          *bool   `access:"environment_developer,write_restrictable"`
+}
+
+type SqlSettings struct {
+	DriverName            *string  `access:"environment_database,write_restrictable"`
+	DataSource            *string  `access:"environment_database,write_restrictable"`
+	DataSourceReplicas    []string `access:"environment_database,write_resitrctable"`
+	MaxOpenConnections    *int     `access:"environment_database,write_restrictable"`
+	ConnectionMaxIdleTime *int     `access:"environment_database,write_restrictable"`
+	ConnectionMaxLifetime *int     `access:"environment_database,write_restrictable"`
+	QueryTimeout          *int     `access:"environment_database,write_restrictable"`
+}
+
+type FileSettings struct {
+	EnableFileAttachments *bool   `access:"environment_file_storage,write_restrictable"`
+	MaxFileSize           *int64  `access:"environment_file_storage,write_restrictable"`
+	Directory             *string `access:"environment_file_storage,write_restrictable"`
+}
+
+type EmailSettings struct {
+	Enable                            *bool   `access:"site_notification"`
+	ReplyToAddress                    *string `access:"site_notification,write_restrictable"`
+	SMTPUsername                      *string `access:"environment_smtp,write_restrictable"`
+	SMTPPassword                      *string `access:"environment_smtp,write_restrictable"`
+	SMTPPort                          *int    `access:"environment_smtp,write_restrictable"`
+	SMTPServerTimeout                 *int    `access:"environment_smtp,write_restrictable"`
+	ConnectionSecurity                *string `access:"environment_smtp,write_restrictable"`
+	SkipServerCertificateVerification *bool   `access:"environment_smtp,write_restrictable"`
+}
+
+type RateLimitSettings struct {
+	Enable *bool `access:"environment_rate_limit,write_restrictable"`
+	Rate   *int  `access:"environment_rate_limit,write_restrictable"`
+}
+
+type OpenIdSettings struct {
+	Enable            *bool   `access:"authentication,environment_openid,write_restrictable"`
+	Secret            *string `access:"authentication,environment_openid,write_restrictable"`
+	Id                *string `access:"authentication,environment_openid,write_restrictable"`
+	Scope             *string `access:"authentication,environment_openid,write_restrictable"`
+	AuthEndpoint      *string `access:"authentication,environment_openid,write_restrictable"`
+	TokenEndpoint     *string `access:"authentication,environment_openid,write_restrictable"`
+	DiscoveryEndpoint *string `access:"authentication,environment_openid,write_restrictable"`
 }
 
 func (s *ServiceSettings) SetDefaults() {
@@ -106,8 +157,56 @@ func (s *ServiceSettings) SetDefaults() {
 	}
 }
 
-type Config struct {
-	ServiceSettings ServiceSettings
+func (s *SqlSettings) SetDefaults() {
+
+}
+
+func (s *FileSettings) SetDefaults() {
+	if s.EnableFileAttachments == nil {
+		s.EnableFileAttachments = NewBool(false)
+	}
+}
+
+func (s *EmailSettings) SetDefaults() {
+	if s.Enable == nil {
+		s.Enable = NewBool(false)
+	}
+
+	if s.SkipServerCertificateVerification == nil {
+		s.SkipServerCertificateVerification = NewBool(false)
+	}
+}
+
+func (s *RateLimitSettings) SetDefaults() {
+	if s.Enable == nil {
+		s.Enable = NewBool(false)
+	}
+}
+
+func (s *OpenIdSettings) SetDefaults() {
+	if s.Enable == nil {
+		s.Enable = NewBool(false)
+	}
+
+	if s.Secret == nil {
+		s.Secret = NewString("")
+	}
+
+	if s.Id == nil {
+		s.Id = NewString("")
+	}
+
+	if s.AuthEndpoint == nil {
+		s.AuthEndpoint = NewString("")
+	}
+
+	if s.TokenEndpoint == nil {
+		s.TokenEndpoint = NewString("")
+	}
+
+	if s.DiscoveryEndpoint == nil {
+		s.DiscoveryEndpoint = NewString("")
+	}
 }
 
 func (o *Config) Clone() *Config {
@@ -131,4 +230,9 @@ func ConfigFromJSON(data io.Reader) *Config {
 
 func (o *Config) SetDefaults() {
 	o.ServiceSettings.SetDefaults()
+	o.SqlSettings.SetDefaults()
+	o.FileSettings.SetDefaults()
+	o.EmailSettings.SetDefaults()
+	o.RateLimitSettings.SetDefaults()
+	o.OpenIdSettings.SetDefaults()
 }
